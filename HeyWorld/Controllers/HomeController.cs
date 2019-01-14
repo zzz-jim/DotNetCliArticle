b50574959991cb5fcd1e702d52ffdd177cf61c66
DESKTOP-BV4QAFB\Administrator <jim.miracle.wang@hotmail.com>
2019-01-14 22:21:07 +0800

    Test version for CLI

---
+++ HeyWorld/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeyWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public string SayHey()
+        {
+            return "Hey, world!";
+        }
+    }
+
+}
